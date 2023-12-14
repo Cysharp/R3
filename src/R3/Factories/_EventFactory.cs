609601726fdc4b5b6c9e4d16ed209cb2f030c582
neuecc <ils@neue.cc>
2023-12-14 13:01:37 +0900

    cancelImmediately

--- src/R3/Factories/_EventFactory.cs
+++ src/R3/Factories/_EventFactory.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Diagnostics.CodeAnalysis;
+using System.Text;
 using System.Threading;
 
 namespace R3;
@@ -30,39 +31,63 @@ public static partial class Event
 
     public static Event<Unit> EveryUpdate()
     {
-        return new EveryUpdate(EventSystem.DefaultFrameProvider, CancellationToken.None);
+        return new EveryUpdate(EventSystem.DefaultFrameProvider, CancellationToken.None, cancelImmediately: false);
     }
 
     public static Event<Unit> EveryUpdate(CancellationToken cancellationToken)
     {
-        return new EveryUpdate(EventSystem.DefaultFrameProvider, cancellationToken);
+        return new EveryUpdate(EventSystem.DefaultFrameProvider, cancellationToken, cancelImmediately: false);
     }
 
     public static Event<Unit> EveryUpdate(FrameProvider frameProvider)
     {
-        return new EveryUpdate(frameProvider, CancellationToken.None);
+        return new EveryUpdate(frameProvider, CancellationToken.None, cancelImmediately: false);
     }
 
     public static Event<Unit> EveryUpdate(FrameProvider frameProvider, CancellationToken cancellationToken)
     {
-        return new EveryUpdate(frameProvider, cancellationToken);
+        return new EveryUpdate(frameProvider, cancellationToken, cancelImmediately: false);
+    }
+
+    public static Event<Unit> EveryUpdate(FrameProvider frameProvider, CancellationToken cancellationToken, bool cancelImmediately)
+    {
+        return new EveryUpdate(frameProvider, cancellationToken, cancelImmediately: cancelImmediately);
     }
 }
 
 
 
-internal sealed class EveryUpdate(FrameProvider frameProvider, CancellationToken cancellationToken) : Event<Unit>
+internal sealed class EveryUpdate(FrameProvider frameProvider, CancellationToken cancellationToken, bool cancelImmediately) : Event<Unit>
 {
     protected override IDisposable SubscribeCore(Subscriber<Unit> subscriber)
     {
-        var runner = new EveryUpdateRunnerWorkItem(subscriber, cancellationToken);
+        var runner = new EveryUpdateRunnerWorkItem(subscriber, cancellationToken, cancelImmediately);
         frameProvider.Register(runner);
         return runner;
     }
 
-    class EveryUpdateRunnerWorkItem(Subscriber<Unit> subscriber, CancellationToken cancellationToken) : IFrameRunnerWorkItem, IDisposable
+    class EveryUpdateRunnerWorkItem : IFrameRunnerWorkItem, IDisposable
     {
+        Subscriber<Unit> subscriber;
+        CancellationToken cancellationToken;
+        CancellationTokenRegistration cancellationTokenRegistration;
         bool isDisposed;
+
+        public EveryUpdateRunnerWorkItem(Subscriber<Unit> subscriber, CancellationToken cancellationToken, bool cancelImmediately)
+        {
+            this.subscriber = subscriber;
+            this.cancellationToken = cancellationToken;
+
+            if (cancelImmediately && cancellationToken.CanBeCanceled)
+            {
+                cancellationTokenRegistration = cancellationToken.UnsafeRegister(static state =>
+                {
+                    var s = (EveryUpdateRunnerWorkItem)state!;
+                    s.subscriber.OnCompleted();
+                    s.Dispose();
+                }, this);
+            }
+        }
 
         public bool MoveNext(long frameCount)
         {
@@ -85,6 +110,7 @@ internal sealed class EveryUpdate(FrameProvider frameProvider, CancellationToken
         public void Dispose()
         {
             isDisposed = true;
+            cancellationTokenRegistration.Dispose();
         }
     }
 }
