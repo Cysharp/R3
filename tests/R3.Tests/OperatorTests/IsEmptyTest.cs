5e148c98df7bb59583ca1f41528fb40927432db6
OWASHI Kazunori <owashi_kazunori@cysharp.co.jp>
2024-01-05 19:47:51 +0900

    add IsEmptyTest

---
+++ tests/R3.Tests/OperatorTests/IsEmptyTest.cs
@@ -0,0 +1,20 @@
+﻿namespace R3.Tests.OperatorTests;
+
+public class IsEmptyTest
+{
+    [Fact]
+    public async Task Positive()
+    {
+        var range = Observable.Empty<int>();
+        var task = range.IsEmptyAsync();
+        (await task).Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task Negative()
+    {
+        var range = Observable.Return(0);
+        var task = range.IsEmptyAsync();
+        (await task).Should().BeFalse();
+    }
+}
