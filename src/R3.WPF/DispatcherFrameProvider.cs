680e5f301e676e52c2a3a4a9041e829e1a7c15c6
neuecc <ils@neue.cc>
2024-01-05 17:31:31 +0900

    WPF

---
+++ src/R3.WPF/DispatcherFrameProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Threading;
+
+namespace R3.WPF;
+
+public sealed class DispatcherFrameProvider : FrameProvider
+{
+
+    public DispatcherFrameProvider(Dispatcher dispatcher)
+    {
+
+        var foo = dispatcher.InvokeAsync(() =>
+        {
+        });
+
+        
+    }
+
+    public override long GetFrameCount()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Register(IFrameRunnerWorkItem callback)
+    {
+        throw new NotImplementedException();
+    }
+}
