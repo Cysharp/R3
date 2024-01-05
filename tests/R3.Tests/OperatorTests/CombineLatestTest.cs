33471be26b49fab8eff897d09882c138f46fb71d
OWASHI Kazunori <owashi_kazunori@cysharp.co.jp>
2024-01-05 20:06:34 +0900

    add merging method tests

---
+++ tests/R3.Tests/OperatorTests/CombineLatestTest.cs
@@ -0,0 +1,52 @@
+﻿namespace R3.Tests.OperatorTests;
+
+public class CombineLatestTest
+{
+    [Fact]
+    public void CombineLatest()
+    {
+        // どちらかに値が入ってきた時点で他方の最新の値とペアにして吐き出す
+
+        var source1 = new Subject<int>();
+        var source2 = new Subject<string>();
+
+        using var list = source1.CombineLatest(source2, ValueTuple.Create).ToLiveList();
+
+        source1.OnNext(1);
+
+        list.AssertEmpty();
+
+        source1.OnNext(2);
+
+        list.AssertEmpty();
+
+        source2.OnNext("a");
+
+        list.AssertEqual([(2, "a")]);
+
+        source1.OnNext(3);
+
+        list.AssertEqual([(2, "a"), (3, "a")]);
+
+        source2.OnNext("b");
+
+        list.AssertEqual([(2, "a"), (3, "a"), (3, "b")]);
+
+        source2.OnNext("c");
+
+        list.AssertEqual([(2, "a"), (3, "a"), (3, "b"), (3, "c")]);
+
+        source1.OnCompleted();
+
+        list.AssertIsNotCompleted();
+
+        source1.OnNext(4);
+        source2.OnNext("d");
+
+        list.AssertEqual([(2, "a"), (3, "a"), (3, "b"), (3, "c"), (3, "d")]);
+
+        source2.OnCompleted();
+
+        list.AssertIsCompleted();
+    }
+}
---
+++ tests/R3.Tests/OperatorTests/WithLatestFromTest.cs
@@ -0,0 +1,43 @@
+﻿namespace R3.Tests.OperatorTests;
+
+public class WithLatestFromTest
+{
+    [Fact]
+    public void WithLatestFrom()
+    {
+        // 最初のストリームに値が発行されたタイミングで、2 番目のストリームの最新の値を併せて出力する
+
+        var source1 = new Subject<int>();
+        var source2 = new Subject<string>();
+
+        using var list = source1.WithLatestFrom(source2, ValueTuple.Create).ToLiveList();
+
+        source1.OnNext(1);
+
+        list.AssertEmpty();
+
+        source1.OnNext(2);
+
+        list.AssertEmpty();
+
+        source2.OnNext("a");
+
+        list.AssertEmpty();
+
+        source1.OnNext(3);
+
+        list.AssertEqual([(3, "a")]);
+
+        source2.OnNext("b");
+
+        list.AssertEqual([(3, "a")]);
+
+        source2.OnNext("c");
+
+        list.AssertEqual([(3, "a")]);
+
+        source1.OnCompleted();
+
+        list.AssertIsCompleted();
+    }
+}
