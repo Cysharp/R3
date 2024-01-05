0c316db84bedfd92b9dcca58e53dd1d753934a4c
OWASHI Kazunori <owashi_kazunori@cysharp.co.jp>
2024-01-05 19:46:35 +0900

    add AllTest

---
+++ tests/R3.Tests/OperatorTests/AllTest.cs
@@ -0,0 +1,20 @@
+﻿namespace R3.Tests.OperatorTests;
+
+public class AllTest
+{
+    [Fact]
+    public async Task Positive()
+    {
+        var range = Observable.Range(1, 10);
+        var task = range.AllAsync(static x => x is > 0 and <= 10);
+        (await task).Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task Negative()
+    {
+        var range = Observable.Range(1, 10);
+        var task = range.AllAsync(static x => x is > 0 and < 10);
+        (await task).Should().BeFalse();
+    }
+}
