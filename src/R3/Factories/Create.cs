e3bb390531240b6a5088477556e222f2a487e9d1
TORISOUP <tori.birdstrike@gmail.com>
2024-01-29 04:03:02 +0900

    add async Observable.Create

--- src/R3/Factories/Create.cs
+++ src/R3/Factories/Create.cs
@@ -11,6 +11,16 @@ public static partial class Observable
     {
         return new AnonymousObservable<T, TState>(state, subscribe, rawObserver);
     }
+
+    public static Observable<T> Create<T>(Func<Observer<T>, CancellationToken, ValueTask> subscribe, bool rawObserver = false)
+    {
+        return new AsyncAnonymousObservable<T>(subscribe, rawObserver);
+    }
+
+    public static Observable<T> Create<T, TState>(TState state, Func<Observer<T>, TState, CancellationToken, ValueTask> subscribe, bool rawObserver = false)
+    {
+        return new AsyncAnonymousObservable<T, TState>(state, subscribe, rawObserver);
+    }
 }
 
 internal sealed class AnonymousObservable<T>(Func<Observer<T>, IDisposable> subscribe, bool rawObserver) : Observable<T>
@@ -26,5 +36,27 @@ internal sealed class AnonymousObservable<T, TState>(TState state, Func<Observer
     protected override IDisposable SubscribeCore(Observer<T> observer)
     {
         return subscribe(rawObserver ? observer : observer.Wrap(), state);
+    }
+}
+
+internal sealed class AsyncAnonymousObservable<T>(Func<Observer<T>, CancellationToken, ValueTask> subscribe, bool rawObserver)
+    : Observable<T>
+{
+    protected override IDisposable SubscribeCore(Observer<T> observer)
+    {
+        var cancellationDisposable = new CancellationDisposable();
+        subscribe(rawObserver ? observer : observer.Wrap(), cancellationDisposable.Token);
+        return cancellationDisposable;
+    }
+}
+
+internal sealed class AsyncAnonymousObservable<T, TState>(TState state, Func<Observer<T>, TState, CancellationToken, ValueTask> subscribe, bool rawObserver)
+    : Observable<T>
+{
+    protected override IDisposable SubscribeCore(Observer<T> observer)
+    {
+        var cancellationDisposable = new CancellationDisposable();
+        subscribe(rawObserver ? observer : observer.Wrap(), state, cancellationDisposable.Token);
+        return cancellationDisposable;
     }
 }
--- tests/R3.Tests/FactoryTests/CreateTest.cs
+++ tests/R3.Tests/FactoryTests/CreateTest.cs
@@ -1,5 +1,4 @@
-﻿
-namespace R3.Tests.FactoryTests;
+﻿namespace R3.Tests.FactoryTests;
 
 public class CreateTest
 {
@@ -39,5 +38,97 @@ public class CreateTest
         publisher.OnCompleted();
         list.AssertIsCompleted();
     }
-}
 
+    [Fact]
+    public void AsyncCreate()
+    {
+        var gate = new TaskCompletionSource();
+
+        var source = Observable.Create<int>(async (observer, _) =>
+        {
+            observer.OnNext(1);
+            await gate.Task;
+            observer.OnNext(10);
+            observer.OnCompleted();
+        });
+
+        using var list = source.ToLiveList();
+        list.AssertEqual([1]);
+        gate.SetResult();
+        list.AssertEqual([1, 10]);
+    }
+
+    [Fact]
+    public void AsyncCreateCancel()
+    {
+        var gate = new TaskCompletionSource();
+
+        var list = Observable.Create<int>(async (observer, ct) =>
+            {
+                ct.Register(() => gate.SetCanceled(ct));
+                observer.OnNext(1);
+                await gate.Task;
+                observer.OnNext(2);
+                observer.OnCompleted();
+            })
+            .ToLiveList();
+
+        list.AssertEqual([1]);
+        list.Dispose();
+        gate.Task.Status.Should().Be(TaskStatus.Canceled);
+    }
+
+    [Fact]
+    public void AsyncCreateS()
+    {
+        using var publisher = new Subject<int>();
+        var source = Observable.Create<int, Subject<int>>(publisher,
+            async (observer, state, ct) =>
+            {
+                while (!ct.IsCancellationRequested)
+                {
+                    try
+                    {
+                        var value = await state.FirstAsync(ct);
+                        observer.OnNext(value);
+                    }
+                    catch (Exception ex)
+                    {
+                        observer.OnCompleted(ex);
+                    }
+                }
+            });
+
+        using var list = source.ToLiveList();
+
+        publisher.OnNext(1);
+        list.AssertEqual([1]);
+        publisher.OnNext(10);
+        list.AssertEqual([1, 10]);
+        publisher.OnNext(100);
+        list.AssertEqual([1, 10, 100]);
+
+        publisher.OnCompleted();
+        list.AssertIsCompleted();
+    }
+
+    [Fact]
+    public void AsyncCreateSCancel()
+    {
+        var gate = new TaskCompletionSource();
+
+        var list = Observable.Create<int, TaskCompletionSource>(gate, async (observer, g, ct) =>
+            {
+                ct.Register(() => g.SetCanceled(ct));
+                observer.OnNext(1);
+                await g.Task;
+                observer.OnNext(2);
+                observer.OnCompleted();
+            })
+            .ToLiveList();
+
+        list.AssertEqual([1]);
+        list.Dispose();
+        gate.Task.Status.Should().Be(TaskStatus.Canceled);
+    }
+}
