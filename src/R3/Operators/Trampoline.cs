ecdf9d425f04903ecb50359dfab4ffa9e97c1e6c
neuecc <ils@neue.cc>
2024-02-09 15:31:51 +0900

    Add Observable.Trampoline

---
+++ src/R3/Operators/Trampoline.cs
@@ -0,0 +1,110 @@
+﻿namespace R3;
+
+public static partial class ObservableExtensions
+{
+    /// <summary>
+    /// Similar as ObserveOn(CurrentThreadScheduler) in dotnet/reactive, place the execution order of recursive calls after the call is completed.
+    /// </summary>
+    public static Observable<T> Trampoline<T>(this Observable<T> source)
+    {
+        return new Trampoline<T>(source);
+    }
+}
+
+internal sealed class Trampoline<T>(Observable<T> source) : Observable<T>
+{
+    protected override IDisposable SubscribeCore(Observer<T> observer)
+    {
+        return source.Subscribe(new _Trampoline(observer));
+    }
+
+    sealed class _Trampoline(Observer<T> observer) : Observer<T>
+    {
+        readonly Queue<Notification<T>> queue = new();
+        bool running;
+
+        protected override bool AutoDisposeOnCompleted => false;
+
+        protected override void OnNextCore(T value)
+        {
+            EnqueueMessage(new(value));
+        }
+
+        protected override void OnErrorResumeCore(Exception error)
+        {
+            EnqueueMessage(new(error));
+        }
+
+        protected override void OnCompletedCore(Result result)
+        {
+            EnqueueMessage(new(result));
+        }
+
+        void EnqueueMessage(Notification<T> notification)
+        {
+            lock (queue)
+            {
+                queue.Enqueue(notification);
+                if (!running)
+                {
+                    running = true;
+                    DrainMessages();
+                }
+            }
+        }
+
+        void DrainMessages()
+        {
+        AGAIN:
+            Notification<T> value;
+            lock (queue)
+            {
+                if (IsDisposed)
+                {
+                    queue.Clear();
+                    return;
+                }
+
+                if (!queue.TryDequeue(out value))
+                {
+                    return;
+                }
+            }
+
+            try
+            {
+                switch (value.Kind)
+                {
+                    case NotificationKind.OnNext:
+                        observer.OnNext(value.Value);
+                        break;
+                    case NotificationKind.OnErrorResume:
+                        observer.OnErrorResume(value.Error);
+                        break;
+                    case NotificationKind.OnCompleted:
+                        try
+                        {
+                            observer.OnCompleted(value.Result);
+                        }
+                        finally
+                        {
+                            Dispose();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    ObservableSystem.GetUnhandledExceptionHandler().Invoke(ex);
+                }
+                catch { }
+            }
+
+            goto AGAIN;
+        }
+    }
+}
---
+++ tests/R3.Tests/OperatorTests/TrampolineTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R3.Tests.OperatorTests;
+
+public class TrampolineTest
+{
+    [Fact]
+    public void NonTrampoline()
+    {
+        var sender = new Subject<string>();
+        var receiver = sender.Share();
+
+        var log = new List<string>();
+
+        // A
+        receiver.Subscribe(x =>
+        {
+            log.Add($"A : {x}");
+            if (x == "OnPlayerJoined") sender.OnNext("OnPlayerAddTeam");
+        });
+
+        // B
+        receiver.Subscribe(x => log.Add($"B : {x}"));
+
+        // C
+        receiver.Subscribe(x => log.Add($"C : {x}"));
+
+        sender.OnNext("OnPlayerJoined");
+
+        var msg = string.Join(Environment.NewLine, log);
+
+        msg.Should().Be("""
+A : OnPlayerJoined
+A : OnPlayerAddTeam
+B : OnPlayerAddTeam
+C : OnPlayerAddTeam
+B : OnPlayerJoined
+C : OnPlayerJoined
+""");
+    }
+
+    [Fact]
+    public void Trampoline()
+    {
+        var sender = new Subject<string>();
+        var receiver = sender.Trampoline().Share();
+
+        var log = new List<string>();
+
+        // A
+        receiver.Subscribe(x =>
+        {
+            log.Add($"A : {x}");
+            if (x == "OnPlayerJoined") sender.OnNext("OnPlayerAddTeam");
+        });
+
+        // B
+        receiver.Subscribe(x => log.Add($"B : {x}"));
+
+        // C
+        receiver.Subscribe(x => log.Add($"C : {x}"));
+
+        sender.OnNext("OnPlayerJoined");
+
+        var msg = string.Join(Environment.NewLine, log);
+
+        msg.Should().Be("""
+A : OnPlayerJoined
+B : OnPlayerJoined
+C : OnPlayerJoined
+A : OnPlayerAddTeam
+B : OnPlayerAddTeam
+C : OnPlayerAddTeam
+""");
+    }
+}
