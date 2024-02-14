b110cd3ef1676ad757b8fb8fc94daa42a869e05f
neuecc <ils@neue.cc>
2024-02-15 05:50:16 +0900

    Add Index

---
+++ tests/R3.Tests/OperatorTests/IndexTest.cs
@@ -0,0 +1,42 @@
+﻿namespace R3.Tests.OperatorTests;
+
+public class IndexTest
+{
+    [Fact]
+    public void Unit()
+    {
+        var subject = new Subject<Unit>();
+        using var list = subject.Index().ToLiveList();
+
+        subject.OnNext(default);
+        subject.OnNext(default);
+        subject.OnNext(default);
+        subject.OnNext(default);
+        subject.OnNext(default);
+
+        list.AssertEqual([0, 1, 2, 3, 4]);
+
+        subject.Dispose();
+
+        list.AssertIsCompleted();
+    }
+
+    [Fact]
+    public void Item()
+    {
+        var subject = new Subject<string>();
+        using var list = subject.Index().ToLiveList();
+
+        subject.OnNext("a");
+        subject.OnNext("b");
+        subject.OnNext("c");
+        subject.OnNext("d");
+        subject.OnNext("e");
+
+        list.AssertEqual([(0, "a"), (1, "b"), (2, "c"), (3, "d"), (4, "e")]);
+
+        subject.Dispose();
+
+        list.AssertIsCompleted();
+    }
+}
