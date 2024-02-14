9f785d6fd8145701abb9ba5dabc39298c52fe18b
neuecc <ils@neue.cc>
2024-02-14 13:05:57 +0900

    (double Delta, T Item)

--- src/R3.Godot/addons/R3.Godot/GodotObservableExtensions.cs
+++ src/R3.Godot/addons/R3.Godot/GodotObservableExtensions.cs
@@ -14,12 +14,12 @@ public static class GodotObservableExtensions
         return new Delta(source, frameProvider);
     }
 
-    public static Observable<(T Value, double Delta)> Delta<T>(this Observable<T> source)
+    public static Observable<(double Delta, T Item)> Delta<T>(this Observable<T> source)
     {
         return Delta(source, GodotFrameProvider.Process);
     }
 
-    public static Observable<(T Value, double Delta)> Delta<T>(this Observable<T> source, GodotFrameProvider frameProvider)
+    public static Observable<(double Delta, T Item)> Delta<T>(this Observable<T> source, GodotFrameProvider frameProvider)
     {
         return new Delta<T>(source, frameProvider);
     }
@@ -51,18 +51,18 @@ internal sealed class Delta(Observable<Unit> source, GodotFrameProvider framePro
     }
 }
 
-internal sealed class Delta<T>(Observable<T> source, GodotFrameProvider frameProvider) : Observable<(T Value, double Delta)>
+internal sealed class Delta<T>(Observable<T> source, GodotFrameProvider frameProvider) : Observable<(double Delta, T Item)>
 {
-    protected override IDisposable SubscribeCore(Observer<(T Value, double Delta)> observer)
+    protected override IDisposable SubscribeCore(Observer<(double Delta, T Item)> observer)
     {
         return source.Subscribe(new _Delta(observer, frameProvider));
     }
 
-    sealed class _Delta(Observer<(T Value, double Delta)> observer, GodotFrameProvider frameProvider) : Observer<T>
+    sealed class _Delta(Observer<(double Delta, T Item)> observer, GodotFrameProvider frameProvider) : Observer<T>
     {
         protected override void OnNextCore(T value)
         {
-            observer.OnNext((value, frameProvider.Delta.Value));
+            observer.OnNext((frameProvider.Delta.Value, value));
         }
 
         protected override void OnErrorResumeCore(Exception error)
