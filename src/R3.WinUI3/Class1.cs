0fb3ccf42855831c75ab2f8297d7d3107e882d9f
neuecc <ils@neue.cc>
2024-02-16 11:48:24 +0900

    WinUI

---
+++ src/R3.WinUI3/Class1.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace R3.WinUI3;
+public class Class1
+{
+}
