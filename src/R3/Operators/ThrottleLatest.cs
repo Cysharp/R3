9433b4d3b42bb5e431c0c4810e93b492779fbf70
neuecc <ils@neue.cc>
2024-02-20 17:10:24 +0900

    Add ThrottleLatest

---
+++ src/R3/Operators/ThrottleLatest.cs
@@ -0,0 +1,269 @@
+﻿namespace R3;
+
+public static partial class ObservableExtensions
+{
+    public static Observable<T> ThrottleLatest<T>(this Observable<T> source, TimeSpan timeSpan)
+    {
+        return new ThrottleLatest<T>(source, timeSpan, ObservableSystem.DefaultTimeProvider);
+    }
+
+    public static Observable<T> ThrottleLatest<T>(this Observable<T> source, TimeSpan timeSpan, TimeProvider timeProvider)
+    {
+        return new ThrottleLatest<T>(source, timeSpan, timeProvider);
+    }
+
+    public static Observable<T> ThrottleLatest<T, TSample>(this Observable<T> source, Observable<TSample> sampler)
+    {
+        return new ThrottleLatestObservableSampler<T, TSample>(source, sampler);
+    }
+
+    public static Observable<T> ThrottleLatest<T>(this Observable<T> source, Func<T, CancellationToken, ValueTask> sampler, bool configureAwait = true)
+    {
+        return new ThrottleLatestAsyncSampler<T>(source, sampler, configureAwait);
+    }
+}
+
+internal sealed class ThrottleLatest<T>(Observable<T> source, TimeSpan interval, TimeProvider timeProvider) : Observable<T>
+{
+    protected override IDisposable SubscribeCore(Observer<T> observer)
+    {
+        return source.Subscribe(new _ThrottleLatest(observer, interval.Normalize(), timeProvider));
+    }
+
+    sealed class _ThrottleLatest : Observer<T>
+    {
+        static readonly TimerCallback timerCallback = RaiseOnNext;
+
+        readonly Observer<T> observer;
+        readonly TimeSpan interval;
+        readonly ITimer timer;
+        readonly object gate = new object();
+        T? lastValue;
+        bool hasValue;
+        bool timerIsRunning;
+
+        public _ThrottleLatest(Observer<T> observer, TimeSpan interval, TimeProvider timeProvider)
+        {
+            this.observer = observer;
+            this.interval = interval;
+            this.timer = timeProvider.CreateStoppedTimer(timerCallback, this);
+        }
+
+        protected override void OnNextCore(T value)
+        {
+            lock (gate)
+            {
+                if (!timerIsRunning) // timer is stopping
+                {
+                    timerIsRunning = true;
+                    timer.InvokeOnce(interval); // timer start before OnNext
+                    observer.OnNext(value);     // call OnNext in lock
+                    return;
+                }
+                else
+                {
+                    hasValue = true;
+                    lastValue = value;
+                }
+            }
+        }
+
+        protected override void OnErrorResumeCore(Exception error)
+        {
+            observer.OnErrorResume(error);
+        }
+
+        protected override void OnCompletedCore(Result result)
+        {
+            observer.OnCompleted(result);
+        }
+
+        protected override void DisposeCore()
+        {
+            timer.Dispose();
+        }
+
+        static void RaiseOnNext(object? state)
+        {
+            var self = (_ThrottleLatest)state!;
+            lock (self.gate)
+            {
+                self.timerIsRunning = false;
+                if (self.hasValue)
+                {
+                    self.observer.OnNext(self.lastValue!);
+                    self.hasValue = false;
+                    self.lastValue = default;
+                }
+            }
+        }
+    }
+}
+
+internal sealed class ThrottleLatestAsyncSampler<T>(Observable<T> source, Func<T, CancellationToken, ValueTask> sampler, bool configureAwait) : Observable<T>
+{
+    protected override IDisposable SubscribeCore(Observer<T> observer)
+    {
+        return source.Subscribe(new _ThrottleLatest(observer, sampler, configureAwait));
+    }
+
+    sealed class _ThrottleLatest(Observer<T> observer, Func<T, CancellationToken, ValueTask> sampler, bool configureAwait) : Observer<T>
+    {
+        readonly object gate = new object();
+        readonly CancellationTokenSource cancellationTokenSource = new();
+        T? lastValue;
+        bool hasValue;
+        Task? taskRunner;
+
+        protected override void OnNextCore(T value)
+        {
+            lock (gate)
+            {
+                if (taskRunner == null)
+                {
+                    taskRunner = RaiseOnNextAsync(value);
+                    observer.OnNext(value);
+                }
+                else
+                {
+                    hasValue = true;
+                    lastValue = value;
+                }
+            }
+        }
+
+        protected override void OnErrorResumeCore(Exception error)
+        {
+            observer.OnErrorResume(error);
+        }
+
+        protected override void OnCompletedCore(Result result)
+        {
+            observer.OnCompleted(result);
+        }
+
+        protected override void DisposeCore()
+        {
+            cancellationTokenSource.Cancel();
+            cancellationTokenSource.Dispose();
+        }
+
+        async Task RaiseOnNextAsync(T value)
+        {
+            try
+            {
+                await sampler(value, cancellationTokenSource.Token).ConfigureAwait(configureAwait);
+            }
+            catch (Exception ex)
+            {
+                if (ex is OperationCanceledException oce && oce.CancellationToken == cancellationTokenSource.Token)
+                {
+                    return;
+                }
+                OnErrorResume(ex);
+            }
+            finally
+            {
+                lock (gate)
+                {
+                    if (hasValue)
+                    {
+                        observer.OnNext(lastValue!);
+                        lastValue = default;
+                        hasValue = false;
+                        taskRunner = null;
+                    }
+                }
+            }
+        }
+    }
+}
+
+internal sealed class ThrottleLatestObservableSampler<T, TSample>(Observable<T> source, Observable<TSample> sampler) : Observable<T>
+{
+    protected override IDisposable SubscribeCore(Observer<T> observer)
+    {
+        return source.Subscribe(new _ThrottleLatest(observer, sampler));
+    }
+
+    sealed class _ThrottleLatest : Observer<T>
+    {
+        readonly Observer<T> observer;
+        readonly object gate = new object();
+        readonly IDisposable samplerSubscription;
+        T? lastValue;
+        bool hasValue;
+        bool closing;
+
+        public _ThrottleLatest(Observer<T> observer, Observable<TSample> sampler)
+        {
+            this.observer = observer;
+            var sampleObserver = new SamplerObserver(this);
+            this.samplerSubscription = sampler.Subscribe(sampleObserver);
+        }
+
+        protected override void OnNextCore(T value)
+        {
+            lock (gate)
+            {
+                if (!closing)
+                {
+                    closing = true;
+                    observer.OnNext(value);
+                }
+                else
+                {
+                    lastValue = value;
+                    hasValue = true;
+                }
+            }
+        }
+
+        protected override void OnErrorResumeCore(Exception error)
+        {
+            observer.OnErrorResume(error);
+        }
+
+        protected override void OnCompletedCore(Result result)
+        {
+            observer.OnCompleted(result);
+        }
+
+        protected override void DisposeCore()
+        {
+            samplerSubscription.Dispose();
+        }
+
+        void PublishOnNext()
+        {
+            lock (gate)
+            {
+                closing = false;
+                if (hasValue)
+                {
+                    observer.OnNext(lastValue!);
+                    hasValue = false;
+                    lastValue = default;
+                }
+            }
+        }
+
+        sealed class SamplerObserver(_ThrottleLatest parent) : Observer<TSample>
+        {
+            protected override void OnNextCore(TSample value)
+            {
+                parent.PublishOnNext();
+            }
+
+            protected override void OnErrorResumeCore(Exception error)
+            {
+                parent.OnErrorResume(error);
+            }
+
+            protected override void OnCompletedCore(Result result)
+            {
+                parent.OnCompleted(result);
+            }
+        }
+    }
+}
---
+++ tests/R3.Tests/OperatorTests/ThrottleLatestTest.cs
@@ -0,0 +1,171 @@
+﻿namespace R3.Tests.OperatorTests;
+
+public class ThrottleLatestTest
+{
+    // ThrottleLatest(TimeSpan)
+    [Fact]
+    public void ThrottleLatest()
+    {
+        var timeProvider = new FakeTimeProvider();
+
+        var publisher = new Subject<int>();
+        var list = publisher.ThrottleLatest(TimeSpan.FromSeconds(3), timeProvider).ToLiveList();
+
+        publisher.OnNext(1);
+        list.AssertEqual([1]);
+
+        publisher.OnNext(10);
+        publisher.OnNext(100);
+        list.AssertEqual([1]);
+
+        timeProvider.Advance(TimeSpan.FromSeconds(2));
+
+        publisher.OnNext(1000);
+        publisher.OnNext(10000);
+        list.AssertEqual([1]);
+
+        timeProvider.Advance(TimeSpan.FromSeconds(1));
+        list.AssertEqual([1, 10000]);
+        publisher.OnNext(2);
+        list.AssertEqual([1, 10000, 2]);
+        publisher.OnNext(20);
+        publisher.OnNext(200);
+
+        timeProvider.Advance(TimeSpan.FromSeconds(3));
+        list.AssertEqual([1, 10000, 2, 200]);
+        publisher.OnNext(3);
+        list.AssertEqual([1, 10000, 2, 200, 3]);
+
+        publisher.OnCompleted();
+
+        list.AssertEqual([1, 10000, 2, 200, 3]);
+        list.AssertIsCompleted();
+    }
+
+    // ThrottleLatest(async)
+    [Fact]
+    public void ThrottleLatestAsyncSampler()
+    {
+        SynchronizationContext.SetSynchronizationContext(null);
+
+        var publisher = new Subject<int>();
+        var fakeTime = new FakeTimeProvider();
+        var list = publisher.ThrottleLatest(async (x, ct) =>
+        {
+            await fakeTime.Delay(TimeSpan.FromSeconds(x), ct);
+        }).ToLiveList();
+
+        publisher.OnNext(1); // gate close
+        list.AssertEqual([1]);
+        publisher.OnNext(2);
+        publisher.OnNext(3);
+
+        list.AssertEqual([1]);
+
+        fakeTime.Advance(1); // gate open
+        list.AssertEqual([1, 3]);
+
+        publisher.OnNext(5);
+        list.AssertEqual([1, 3, 5]);
+
+        publisher.OnNext(6);
+        publisher.OnNext(7);
+
+        fakeTime.Advance(4);
+        list.AssertEqual([1, 3, 5]);
+
+        publisher.OnNext(8);
+
+        list.AssertEqual([1, 3, 5]);
+
+        fakeTime.Advance(1);
+        list.AssertEqual([1, 3, 5, 8]);
+
+        publisher.OnCompleted();
+
+        list.AssertIsCompleted();
+    }
+
+    // ThrottleLatest(Observable)
+    [Fact]
+    public void ThrottleLatestObservableSampler()
+    {
+        SynchronizationContext.SetSynchronizationContext(null);
+
+        var publisher = new Subject<int>();
+        var sampler = new Subject<Unit>();
+        var list = publisher.ThrottleLatest(sampler).ToLiveList();
+
+        publisher.OnNext(1); // gate close
+        list.AssertEqual([1]);
+        publisher.OnNext(2);
+        publisher.OnNext(3);
+
+        list.AssertEqual([1]);
+
+        sampler.OnNext(Unit.Default);
+        list.AssertEqual([1, 3]);
+
+        publisher.OnNext(5);
+        list.AssertEqual([1, 3, 5]);
+        publisher.OnNext(6);
+        publisher.OnNext(7);
+
+        sampler.OnNext(Unit.Default);
+        list.AssertEqual([1, 3, 5, 7]);
+
+
+        publisher.OnNext(8);
+        list.AssertEqual([1, 3, 5, 7, 8]);
+
+        sampler.OnNext(Unit.Default);
+        list.AssertEqual([1, 3, 5, 7, 8]);
+        sampler.OnNext(Unit.Default);
+        sampler.OnNext(Unit.Default);
+        list.AssertEqual([1, 3, 5, 7, 8]);
+
+        publisher.OnCompleted();
+
+        list.AssertIsCompleted();
+    }
+
+
+    // ThrottleLatestFrame
+
+    [Fact]
+    public void ThrottleLatestFrame()
+    {
+        var frameProvider = new FakeFrameProvider();
+
+        var publisher = new Subject<int>();
+        var list = publisher.ThrottleLatestFrame(3, frameProvider).ToLiveList();
+
+        publisher.OnNext(1);
+        list.AssertEqual([1]);
+        publisher.OnNext(10);
+        publisher.OnNext(100);
+        list.AssertEqual([1]);
+
+        frameProvider.Advance(2);
+
+        publisher.OnNext(1000);
+        publisher.OnNext(10000);
+        list.AssertEqual([1]);
+
+        frameProvider.Advance(1);
+        list.AssertEqual([1, 10000]);
+        publisher.OnNext(2);
+        list.AssertEqual([1, 10000, 2]);
+        publisher.OnNext(20);
+        publisher.OnNext(200);
+
+        frameProvider.Advance(3);
+        list.AssertEqual([1, 10000, 2, 200]);
+        publisher.OnNext(3);
+
+        publisher.OnCompleted();
+
+        list.AssertEqual([1, 10000, 2, 200, 3]);
+        list.AssertIsCompleted();
+    }
+}
