4f1bd053bc8204fd3bfda824de03524f71a21823
EXE <expexe.jp@gmail.com>
2024-05-26 09:34:42 +0900

    Add BlazorWebAssembly.

---
+++ sandbox/BlazorWebAssemblyApp1/Pages/Counter.razor.cs
@@ -0,0 +1,24 @@
+﻿using R3;
+
+namespace BlazorWebAssemblyApp1.Pages;
+
+public partial class Counter : IDisposable
+{
+    int currentCount = 0;
+    IDisposable? subscription;
+
+    protected override void OnInitialized()
+    {
+        subscription = Observable.Interval(TimeSpan.FromSeconds(1))
+            .Subscribe(_ =>
+            {
+                currentCount++;
+                StateHasChanged();
+            });
+    }
+
+    public void Dispose()
+    {
+        subscription?.Dispose();
+    }
+}
