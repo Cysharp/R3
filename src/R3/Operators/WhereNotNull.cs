3f16e316b0e93e22c52840cc2eea9e9e3ffd9c57
erri120 <erri120@fossmailer.de>
2024-08-10 19:16:33 +0200

    Add `WhereNotNull` operator
    Using optimized `WhereSelect`, the `WhereNotNull` operator is very
    common and many libraries and applications had to DIY implement
    this for `System.IObservable<T>`.

---
+++ src/R3/Operators/WhereNotNull.cs
@@ -0,0 +1,13 @@
+﻿namespace R3;
+
+public static partial class ObservableExtensions
+{
+    public static Observable<TResult> WhereNotNull<TResult>(this Observable<TResult?> source) where TResult : class
+    {
+        return new WhereSelect<TResult?, TResult>(
+            source: source,
+            selector: static item => item!,
+            predicate: item => item is not null
+        );
+    }
+}
