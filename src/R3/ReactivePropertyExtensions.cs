f7be1bf1ec6d29c7514c31ffa33edb44775e9080
neuecc <ils@neue.cc>
2024-08-15 14:58:52 +0900

    true ReadOnlyBindableReactiveProperty

--- src/R3/ReactivePropertyExtensions.cs
+++ src/R3/ReactivePropertyExtensions.cs
@@ -1,4 +1,9 @@
-﻿namespace R3;
+﻿using System.Collections;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+
+namespace R3;
 
 public static class ReactivePropertyExtensions
 {
@@ -32,12 +37,12 @@ public static class ReactivePropertyExtensions
 
     public static IReadOnlyBindableReactiveProperty<T> ToReadOnlyBindableReactiveProperty<T>(this Observable<T> source, T initialValue = default!)
     {
-        return new BindableReactiveProperty<T>(source, initialValue, EqualityComparer<T>.Default);
+        return new ReadOnlyBindableReactiveProperty<T>(new BindableReactiveProperty<T>(source, initialValue, EqualityComparer<T>.Default));
     }
 
     public static IReadOnlyBindableReactiveProperty<T> ToReadOnlyBindableReactiveProperty<T>(this Observable<T> source, IEqualityComparer<T>? equalityComparer, T initialValue = default!)
     {
-        return new BindableReactiveProperty<T>(source, initialValue, equalityComparer);
+        return new ReadOnlyBindableReactiveProperty<T>(new BindableReactiveProperty<T>(source, initialValue, equalityComparer));
     }
 }
 
@@ -75,3 +80,96 @@ internal sealed class ConnectedReactiveProperty<T> : ReactiveProperty<T>
     }
 }
 
+internal sealed class ReadOnlyBindableReactiveProperty<T>(BindableReactiveProperty<T> property) : IReadOnlyBindableReactiveProperty<T>
+{
+    public T Value => ((IReadOnlyBindableReactiveProperty<T>)property).Value;
+
+    public bool HasErrors => ((INotifyDataErrorInfo)property).HasErrors;
+
+    object? IReadOnlyBindableReactiveProperty.Value => ((IReadOnlyBindableReactiveProperty)property).Value;
+
+    PropertyChangedEventHandler? propertyChangedCore;
+
+    public event PropertyChangedEventHandler? PropertyChanged
+    {
+        add
+        {
+            propertyChangedCore += value;
+            ((INotifyPropertyChanged)property).PropertyChanged += PropertyChangedEventHandler;
+        }
+
+        remove
+        {
+            propertyChangedCore -= value;
+            ((INotifyPropertyChanged)property).PropertyChanged -= PropertyChangedEventHandler;
+        }
+    }
+
+    EventHandler<DataErrorsChangedEventArgs>? errorsChangedCore;
+
+    public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged
+    {
+        add
+        {
+            errorsChangedCore += value;
+            ((INotifyDataErrorInfo)property).ErrorsChanged += ErrorsChangedEventHandler;
+        }
+
+        remove
+        {
+            errorsChangedCore -= value;
+            ((INotifyDataErrorInfo)property).ErrorsChanged -= ErrorsChangedEventHandler;
+        }
+    }
+
+    void PropertyChangedEventHandler(object? sender, PropertyChangedEventArgs e)
+    {
+        // parent sender is BindableReactiveProperty<T>, change to self.
+        propertyChangedCore?.Invoke(this, e);
+    }
+
+    void ErrorsChangedEventHandler(object? sender, DataErrorsChangedEventArgs e)
+    {
+        errorsChangedCore?.Invoke(this, e);
+    }
+
+    public Observable<T> AsObservable()
+    {
+        return ((IReadOnlyBindableReactiveProperty<T>)property).AsObservable();
+    }
+
+    public void Dispose()
+    {
+        ((IDisposable)property).Dispose();
+    }
+
+    public IReadOnlyBindableReactiveProperty<T> EnableValidation()
+    {
+        return ((IReadOnlyBindableReactiveProperty<T>)property).EnableValidation();
+    }
+
+    public IReadOnlyBindableReactiveProperty<T> EnableValidation(Func<T, Exception?> validator)
+    {
+        return ((IReadOnlyBindableReactiveProperty<T>)property).EnableValidation(validator);
+    }
+
+    public IReadOnlyBindableReactiveProperty<T> EnableValidation<TClass>([CallerMemberName] string? propertyName = null)
+    {
+        return ((IReadOnlyBindableReactiveProperty<T>)property).EnableValidation<TClass>(propertyName);
+    }
+
+    public IReadOnlyBindableReactiveProperty<T> EnableValidation(Expression<Func<IReadOnlyBindableReactiveProperty<T>?>> selfSelector)
+    {
+        return ((IReadOnlyBindableReactiveProperty<T>)property).EnableValidation(selfSelector);
+    }
+
+    public IEnumerable GetErrors(string? propertyName)
+    {
+        return ((INotifyDataErrorInfo)property).GetErrors(propertyName);
+    }
+
+    public override string? ToString()
+    {
+        return property.ToString();
+    }
+}
