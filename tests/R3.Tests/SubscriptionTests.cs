959df78c9185fb531919b18e963d375ba626b99a
m.pavlov <m.pavlov@corp.nekki.ru>
2024-08-17 15:22:19 +0300

    Subscribe_ShouldNotThrowException

---
+++ tests/R3.Tests/SubscriptionTests.cs
@@ -0,0 +1,32 @@
+﻿namespace R3.Tests;
+
+public class SubscriptionTests
+{
+    public class TestObservable<T> : Observable<T>, IDisposable
+    {
+        private readonly Subject<T> _subject = new();
+
+        protected override IDisposable SubscribeCore(Observer<T> observer)
+        {
+            return _subject.Subscribe(observer);
+        }
+
+        public void Dispose()
+        {
+            _subject.Dispose();
+        }
+    }
+
+    [Fact]
+    public void Subscribe_ShouldNotThrowException()
+    {
+        var subject = new TestObservable<object>();
+        IDisposable disposable = null;
+        Action action = () =>
+        {
+            disposable = subject.Subscribe(_ => { });
+        };
+        action.Should().NotThrow();
+        disposable?.Dispose();
+    }
+}
